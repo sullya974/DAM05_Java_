f3e7c35b87f056986431c933dc03734ffe75f320
sullya974 <sully.adelard@laposte.net>
2021-05-12 14:42:18 +0200

    Rendre Monnaie

--- JFX_Prj1/src/sample/Controller.java
+++ JFX_Prj1/src/sample/Controller.java
@@ -9,10 +9,7 @@ import javafx.scene.layout.VBox;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Controller {
     @FXML
@@ -86,23 +83,81 @@ public class Controller {
     @FXML
     //private void handlePayer(ActionEvent event){
     private void handlePayer(){
-        double montantPaiement = (txtPaiement.getText() != "") ? Double.parseDouble(txtPaiement.getText()) : 0;
-
         Boisson selectedBoisson = lstBoisson.getSelectionModel().getSelectedItem();
         int selectedIndex = lstBoisson.getSelectionModel().getSelectedIndex();
+
+        int montantPaiement = (txtPaiement.getText() != "") ? Integer.parseInt(txtPaiement.getText())*100 : 0;
+        int montantAPayer = (int) (selectedBoisson.prix*100);
+
         double montantARendre = montantPaiement - selectedBoisson.prix;
 
-        // Cas : Montant payer inférieur au prix - Message : Reste à payer : XXX
-        if(montantPaiement == 0){
-            txtMessagePaiement.setText("Veuillez payer: " + selectedBoisson.prix + " €");
-        }else if(selectedBoisson.prix > montantPaiement){
-            txtMessagePaiement.setText("Monnaie rendue: " + montantARendre + " €\nBonne dégustation ;)");
-        }else if(selectedBoisson.prix < montantPaiement){ // Cas : Montant payer supérieur au prix - Message : Monnaie rendue: XXX
-            txtMessagePaiement.setText("Monnaie rendue: " + montantARendre + " €\nBonne dégustation ;)");
-        }else{
-            txtMessagePaiement.setText("Bonne dégustation ;)");
+        List<Integer> lstMonnaieRendue = ManageMonaieARendre(montantPaiement, montantAPayer);
+        System.out.println("============================");
+        for(int i : lstMonnaieRendue){
+            System.out.println(i);
         }
-        //Enlève la boisson sélectionnée de la liste
-        lstBoisson.getItems().remove(selectedIndex);
+
+//        // Cas : Montant payer inférieur au prix - Message : Reste à payer : XXX
+//        if(montantPaiement == 0){
+//            txtMessagePaiement.setText("Veuillez payer: " + selectedBoisson.prix + " €");
+//        }else if(selectedBoisson.prix > montantPaiement){
+//            double resteAPayer = selectedBoisson.prix - montantPaiement;
+//            txtMessagePaiement.setText("Reste à payer: " + resteAPayer + " €");
+//        }else if(selectedBoisson.prix < montantPaiement){ // Cas : Montant payer supérieur au prix - Message : Monnaie rendue: XXX
+//            txtMessagePaiement.setText("Monnaie rendue: " + montantARendre + " €\nBonne dégustation ;)");
+//            //Enlève la boisson sélectionnée de la liste
+//            lstBoisson.getItems().remove(selectedIndex);
+//        }else{
+//            txtMessagePaiement.setText("Bonne dégustation ;)");
+//        }
+    }
+
+    // TODO: Vérifier que paiement >= prix
+
+    /**
+     *
+     * @param montantPaiement: Montant du paiement
+     * @param montantAPayer: Prix à payer
+     */
+    List<Integer> getListMonnaieRendue(int montant, int aRendre, List<Integer> lstMonnaieRendue)
+    {
+        if(aRendre >= montant && aRendre%montant >= 0){
+            lstMonnaieRendue.add(montant);
+            aRendre -= montant;
+        }
+        return lstMonnaieRendue;
+    }
+    List<Integer> ManageMonaieARendre(int montantPaiement, int montantAPayer){
+        int aRendre = montantPaiement - montantAPayer;
+        List<Integer> lstMonnaieRendue = new ArrayList<Integer>();
+        List<Integer> lstTypePieces = new ArrayList<Integer>(Arrays.asList(1, 2, 5, 10, 20, 50, 100, 200));
+        Collections.sort(lstTypePieces, Collections.reverseOrder());
+        System.out.println("Paiement : " + montantPaiement);
+        System.out.println("A payer : " + montantAPayer);
+        System.out.println(">>> A rendre : " + aRendre);
+
+
+        for(int i : lstTypePieces){
+            System.out.println("- A rendre : " + aRendre);
+            System.out.println(">>> " + i);
+            System.out.println("Modulo:" + aRendre%i);
+            if(aRendre >= i && aRendre%i >= 0){
+
+                lstMonnaieRendue.add(i);
+                aRendre -= i;
+            }
+        }
+        return lstMonnaieRendue;
+//
+//
+//
+//        // Tant que aRendre > 0
+//        while(aRendre > 0) {
+//            // Récupère le montant max à déduire de aRendre à partir de la liste des types
+//            int montantADeduire = lstTypePieces.stream().
+//            // Calcul le type de pièce à rendre
+//            // > Nb Type Pièce = aRendre /
+//            // Ajoute le type de pièce à rendre dans la liste monnaie rendue
+//        }
     }
 }
